5aee5b8dd1ad4cee628ec98c81f549d7bb0b241c
finaorivia19 <100359144+finaorivia19@users.noreply.github.com>
2022-06-09 19:58:21 +0700

    Add files via upload

---
+++ Pertemuan 13/BinaryTree.java
@@ -0,0 +1,215 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Node;
+
+/**
+ *
+ * @author USER
+ */
+public class BinaryTree {
+    Node root;
+    int total = 0;
+    
+    public BinaryTree() {
+        root = null;
+    }
+    boolean isEmpty() {
+        return root == null;
+    }
+    //tugas praktikum 1
+    void addrekursif(int key) {
+        root = insertRec(root, key);
+    }
+    
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.data) {
+            root.right = insertRec(root.right, key);
+        }
+        return root;
+    }
+  
+  void add (int data){
+      if(isEmpty()){
+          root = new Node(data);
+      }else{
+          Node current = root;
+          while(true){
+            if(data<current.data){
+                if(current.left!=null){
+                    current = current.left;
+                }else{
+                    current.left = new Node(data);
+                    break;
+                }
+          }else if(data>current.data){
+              if(current.right!=null){
+                  current = current.right;
+              }else{
+                  current.right = new Node(data);
+                  break;
+              }
+          }else{
+              break;
+          }
+          }
+      }
+  }
+  boolean find(int data) {
+        boolean hasil = false;
+        Node current = root;
+        while (current != null) {
+            if (current.data == data) {
+                hasil = true;
+                break;
+            } else if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return hasil;
+    }
+  void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+  void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+  //tugas praktikum 5
+  void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+  Node getSuccessor(Node del) {
+        Node successor = del.right;
+        Node successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successorParent.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+        }
+        return successor;
+    }
+  void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree Is Empty!");
+            return;
+        }
+        //find node (current) that will be deleted
+        Node parent = root;
+        Node current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (current.data == data) {
+                break;
+            } else if (data < current.data) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (data > current.data) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        //delete
+    if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        } else {
+            //if there is no child, simply delete it
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) {// if there is 1 child (right)
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {// if there is 1 child (left)
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {//if there is 2 child
+                Node successor = getSuccessor(current);
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    }
+                    successor.left = current.left;
+                }
+            }
+        }
+    }
+// tugas praktikum 2
+  int Min(Node node) { 
+        Node current = root;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    int Max(Node node) {
+        Node current = root;
+        while (current.right != null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+    //tugas praktikum 3
+    public void printLeaves(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.isLeaf()) {
+            System.out.print(" " + node.data);
+            total++;
+        }
+        printLeaves(node.left);
+        printLeaves(node.right);
+    }
+}
